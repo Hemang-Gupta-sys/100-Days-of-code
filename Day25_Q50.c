c896d7289b9e2018658b80aaf804b02d823fead7
Hemang-Gupta-sys <hemanggupta961@gmail.com>
2025-11-17 21:46:20 +0530

    Added

---
+++ Day25_Q50.c
@@ -0,0 +1,45 @@
+//Q50: Write a program to print the following pattern:
+/*  *****
+     ****
+      *** 
+       **
+        *
+*/
+
+#include <stdio.h>
+int main() {
+    int i, j, n = 5;
+
+    for (i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (j = 1; j < i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = i; j <= n; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+ ****
+  ***
+   **
+    *
+
+Input 2:
+
+Output 2:
+Note: Spaces indicate indentation.
+
+*/
